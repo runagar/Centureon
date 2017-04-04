41a19ea484287e085c25a5a05dbb97346835d874
Runagar <runagar@gmail.com>
2017-04-04 11:48:13 +0200

    balls to the walls

--- Assets/Scripts/Behaviours/Enemies/Attack/EnemyMelee.cs
+++ Assets/Scripts/Behaviours/Enemies/Attack/EnemyMelee.cs
@@ -10,6 +10,9 @@ public class EnemyMelee : MonoBehaviour {
     Vector3 attackDirection;
     GameObject player;
 
+    EnemyTelegraphs telegraphScript;
+    List<Vector2> targetTiles;
+
     // Use this for initialization
     void Start () {
         isChargingAttack = false;
@@ -17,26 +20,50 @@ public class EnemyMelee : MonoBehaviour {
         stats = this.GetComponent<UnitStats>();
 
         player = GameObject.FindGameObjectWithTag("PLAYER");
+        telegraphScript = this.GetComponent<EnemyTelegraphs>();
+        targetTiles = new List<Vector2>();
     }
 	
 	public void ChargeAttack(Vector3 attackDir)
     {
         attackDirection = attackDir;
         isChargingAttack = true;
+
+        targetTiles.Clear();
+
+        for (int i = 0; i < stats.range; i++)
+        {
+            if (attackDirection.x == 0 && Mathf.Sign(attackDirection.z) == -1) targetTiles.Add(new Vector2(0, -(i + 1)));
+            if (attackDirection.x == 0 && Mathf.Sign(attackDirection.z) == 1) targetTiles.Add(new Vector2(0, i + 1));
+            if (attackDirection.z == 0 && Mathf.Sign(attackDirection.x) == -1) targetTiles.Add(new Vector2(-(i + 1), 0));
+            if (attackDirection.z == 0 && Mathf.Sign(attackDirection.x) == 1) targetTiles.Add(new Vector2((i + 1), 0));
+        }
+
+        telegraphScript.startTelegraph(targetTiles);
     }
 
     public void ConcludeAttack()
     {
         if (player != null && stats.isKill == "no") {
 
-            Vector2 p = new Vector2(player.transform.position.x, player.transform.position.z);
-            Vector2 a = new Vector2(this.transform.position.x + attackDirection.x, this.transform.position.z + attackDirection.z);
+            Vector2 playerPos = new Vector2(player.transform.position.x, player.transform.position.z);
+            Vector2 attackPos = new Vector2(this.transform.position.x + attackDirection.x, this.transform.position.z + attackDirection.z);
 
-            if (p == a && this.gameObject != null) {
+            if (playerPos == attackPos && this.gameObject != null) {
                 Destroy(player.gameObject);
             }
         }
+        targetTiles.Clear();
 
+        for (int i = 0; i < stats.range; i++)
+        {
+            if (attackDirection.x == 0 && Mathf.Sign(attackDirection.z) == -1) targetTiles.Add(new Vector2(0, -(i + 1)));
+            if (attackDirection.x == 0 && Mathf.Sign(attackDirection.z) == 1) targetTiles.Add(new Vector2(0, i + 1));
+            if (attackDirection.z == 0 && Mathf.Sign(attackDirection.x) == -1) targetTiles.Add(new Vector2(-(i + 1), 0));
+            if (attackDirection.z == 0 && Mathf.Sign(attackDirection.x) == 1) targetTiles.Add(new Vector2((i + 1), 0));
+        }
+
+        telegraphScript.endTelegraph(targetTiles);
 
         isChargingAttack = false;
     }
---
+++ Assets/Scripts/Behaviours/Enemies/Attack/EnemyTelegraphs.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTelegraphs : MonoBehaviour {
+
+	public void startTelegraph(List<Vector2> targetTiles)
+    {
+        print("Attacking: " + targetTiles);
+    }
+
+    public void endTelegraph(List<Vector2> targetTiles)
+    {
+        print("Hit: " + targetTiles);
+    }
+}
