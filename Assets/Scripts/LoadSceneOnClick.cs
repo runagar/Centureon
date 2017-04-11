73d7e3c2f4d8488b21d1d693749b788ac89563cd
Yoh874 <micuks1@gmail.com>
2017-04-11 13:45:39 +0200

    Add scene changing

---
+++ Assets/Scripts/LoadSceneOnClick.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadSceneOnClick : MonoBehaviour {
+
+	static int selected = 0;
+
+    //mainMenuVariables varKeeper;
+	Button Mainmenu;
+	//public Sprite MainMenuSprite;
+
+
+    void Start()
+    {
+	    //varKeeper = GameObject.Find("KeeperOfVariables").GetComponent<mainMenuVariables>();
+		Mainmenu = GetComponent<Button>();
+		//Mainmenu.image.sprite = MainMenuSprite;
+    }
+
+	public void LoadByIndex(int sceneIndex) {
+		SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void loadMainMenu()
+    {
+        SceneManager.LoadScene("mainMenu");
+    }
+
+    public void dnd()
+    {
+  //      varKeeper.useDragonDrop = true;
+    }
+    public void text()
+    {
+//        varKeeper.useDragonDrop = false;
+    }
+}
