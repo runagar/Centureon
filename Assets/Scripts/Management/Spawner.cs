08e8d3de26ebdadc2f7481358e6e55dd49e8b1a9
Bjacob93 <Zeint@live.dk>
2017-04-11 10:03:06 +0200

    enemy spawning and defeat fixed
    the fix for resetting a level is rather crude

--- Assets/Scripts/Management/Spawner.cs
+++ Assets/Scripts/Management/Spawner.cs
@@ -8,22 +8,41 @@ public class Spawner : MonoBehaviour {
     SimpleMapGridCreation mapLayout;
 
     //Cache the prefabs for the units
-    public GameObject player, meleeEnemy, rangedEnemy;
+    public GameObject player, swordEnemy, spearEnemy;
 
     //The object all units should be children of
     GameObject turnTracker;
+
+    //Cache the current level we are in 
+    int currentLevel;
 
     private void Start()
     {
         //Reference the map layout script
         mapLayout = GameObject.Find("MapLayout").GetComponent<SimpleMapGridCreation>();
         turnTracker = GameObject.Find("TurnTracker");
+        currentLevel = GameObject.Find("LevelManager").GetComponent<levelManager>().getCurrentLevel();
 
-        Instantiate(player, new Vector3(0, 0.25f, 0), Quaternion.identity, turnTracker.transform);
-        Instantiate(meleeEnemy, new Vector3(9, 0.25f, 2), Quaternion.identity, turnTracker.transform);
-        Instantiate(rangedEnemy, new Vector3(3, 0.25f, 7), Quaternion.identity, turnTracker.transform);
-        Instantiate(meleeEnemy, new Vector3(8, 0.25f, 7), Quaternion.identity, turnTracker.transform);
-        Instantiate(rangedEnemy, new Vector3(5, 0.25f, 5), Quaternion.identity, turnTracker.transform);
-
+        if (currentLevel == 0){
+            Instantiate(player, new Vector3(0, 0.25f, 0), Quaternion.identity, turnTracker.transform);
+            Instantiate(swordEnemy, new Vector3(9, 0.25f, 2), Quaternion.identity, turnTracker.transform);
+            Instantiate(swordEnemy, new Vector3(3, 0.25f, 7), Quaternion.identity, turnTracker.transform);
+            Instantiate(swordEnemy, new Vector3(8, 0.25f, 7), Quaternion.identity, turnTracker.transform);
+            Instantiate(swordEnemy, new Vector3(5, 0.25f, 5), Quaternion.identity, turnTracker.transform);
+        }
+        else if (currentLevel == 1){
+            Instantiate(player, new Vector3(0, 0.25f, 0), Quaternion.identity, turnTracker.transform);
+            Instantiate(swordEnemy, new Vector3(9, 0.25f, 2), Quaternion.identity, turnTracker.transform);
+            Instantiate(swordEnemy, new Vector3(3, 0.25f, 7), Quaternion.identity, turnTracker.transform);
+            Instantiate(spearEnemy, new Vector3(8, 0.25f, 5), Quaternion.identity, turnTracker.transform);
+            Instantiate(spearEnemy, new Vector3(4, 0.25f, 2), Quaternion.identity, turnTracker.transform);
+        }
+        else{
+            Instantiate(player, new Vector3(0, 0.25f, 0), Quaternion.identity, turnTracker.transform);
+            Instantiate(spearEnemy, new Vector3(0, 0.25f, 9), Quaternion.identity, turnTracker.transform);
+            Instantiate(spearEnemy, new Vector3(6, 0.25f, 7), Quaternion.identity, turnTracker.transform);
+            Instantiate(spearEnemy, new Vector3(9, 0.25f, 2), Quaternion.identity, turnTracker.transform);
+            Instantiate(spearEnemy, new Vector3(5, 0.25f, 3), Quaternion.identity, turnTracker.transform);
+        }
     }
 }
--- Assets/Scripts/Management/VictoryDefeatCondition.cs
+++ Assets/Scripts/Management/VictoryDefeatCondition.cs
@@ -12,25 +12,40 @@ public class VictoryDefeatCondition : MonoBehaviour {
     // Use this for initialization
     void Start () {
         turntracker = GameObject.Find("TurnTracker").GetComponent<TurnTracker>();
-        player = GameObject.FindGameObjectWithTag("PLAYER");
         currentIndex = this.GetComponent<levelManager>().getCurrentLevel();
         didPlayerMove = false;
+        player = GameObject.FindGameObjectWithTag("PLAYER");
     }
 	
 	// Update is called once per frame
 	void Update () {
-		if(player == null)
+        player = GameObject.FindGameObjectWithTag("PLAYER");
+        if (player == null & didPlayerMove == true)
         {
             Debug.Log("defeated");
             //defeat
-            SceneManager.LoadScene(0);
+            //SceneManager.LoadScene(currentIndex);
+            reload();
 
         }
         if(turntracker.enemies.Length <= 0 && didPlayerMove == true)
         {
             Debug.Log("victory");
             //victory
-            SceneManager.LoadScene(currentIndex + 1);
+            if (currentIndex == 2){
+                SceneManager.LoadScene(0);
+            }
+            else {
+                SceneManager.LoadScene(currentIndex + 1);
+            }
         }
 	}
+
+    public void reload()
+    {
+        int scene = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(scene, LoadSceneMode.Single);
+        Time.timeScale = 1;
+        didPlayerMove = false;
+    }
 }
