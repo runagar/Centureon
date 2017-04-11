67351b36fad628caa5faf998346445762e28dc79
Yoh874 <micuks1@gmail.com>
2017-04-11 15:21:29 +0200

    Hover stuff works

--- Assets/Scripts/MouseOver.cs
+++ Assets/Scripts/MouseOver.cs
@@ -8,38 +8,29 @@ using System.Collections;
 
 public class MouseOver : MonoBehaviour
 {
-	Ray ray;
-	RaycastHit hit;
 	_2dxFX_AL_Outline outline;
 	// Use this for initialization
 	void Start()
 	{
-
-	
-
-		outline= GetComponent<_2dxFX_AL_Outline>();
+		outline = GameObject.Find("Start").GetComponent<_2dxFX_AL_Outline>();
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-		ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-		if (Physics.Raycast(ray, out hit))
-		{
-			outline._OutLineSpread = 0.01f;
-			print(hit.collider.name);
-		}
-		else
-			outline._OutLineSpread = 0.02f;
-	
+		
 	}
 
 	void OnMouseEnter()
 
 	{
-		
+		outline.enabled=true;	
+		//outline._OutLineSpread = 0.02f;
 		print(gameObject.name);
+	}
+	void OnMouseExit()
+	{
+		outline.enabled = false;
 
 	}
 
