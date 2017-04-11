a08c58988d50fcbd39acb64b4453b0a67f7160d9
Yoh874 <micuks1@gmail.com>
2017-04-11 14:44:01 +0200

    Main menu working now

---
+++ Assets/Scripts/QuitOnClick.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class QuitOnClick : MonoBehaviour {
+	Button Mainmenu;
+
+	void Update() {
+
+	}
+	void Start() { 
+		Mainmenu = GetComponent<Button>();
+	}
+	public void OnMouseDown()
+	{
+		print("hi");
+		Quit();
+	}
+	void Quit() { 
+			Application.Quit();
+
+	}
+}
